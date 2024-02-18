1ac8b6fbf89b5f6c1d3d644973ee693aca4c2a54
Max Rakitin <mrakitin@bnl.gov>
2024-02-17 23:29:03 -0500

    TST: add input arg for a number of frames to the testing script

--- scripts/test-file-saving.sh
+++ scripts/test-file-saving.sh
@@ -3,6 +3,8 @@
 # shellcheck source=/dev/null
 . /etc/profile.d/epics.sh
 
+num="${1:-50}"
+
 data_dir="/tmp/test/$(date +%Y/%m/%d)"
 
 if [ ! -d "${data_dir}" ]; then
@@ -12,7 +14,7 @@ fi
 caput "BASE:{Dev:Save1}:write_dir" "${data_dir}"
 caput "BASE:{Dev:Save1}:file_name" "saveme_{num:06d}_{uid}.h5"
 caput "BASE:{Dev:Save1}:stage" 1
-for i in $(seq 50); do
+for i in $(seq "$num"); do
     echo "$i"
     sleep 0.1
     caput "BASE:{Dev:Save1}:acquire" 1
