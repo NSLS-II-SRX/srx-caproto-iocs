cfe47e2db45b619cd2849c98e1d04a72151155dc
Max Rakitin <mrakitin@bnl.gov>
2024-02-21 19:39:14 -0500

    Parametrize the script to start different caproto IOCs

--- scripts/run-caproto-ioc.sh
+++ scripts/run-caproto-ioc.sh
@@ -2,6 +2,8 @@
 
 set -vxeuo pipefail
 
+CAPROTO_IOC="${1:-srx_caproto_iocs.base}"
+
 # shellcheck source=/dev/null
 if [ -f "/etc/profile.d/epics.sh" ]; then
     . /etc/profile.d/epics.sh
@@ -10,4 +12,4 @@ fi
 export EPICS_CAS_AUTO_BEACON_ADDR_LIST="no"
 export EPICS_CAS_BEACON_ADDR_LIST="${EPICS_CA_ADDR_LIST:-127.0.0.255}"
 
-python -m srx_caproto_iocs.base --prefix="BASE:{{Dev:Save1}}:" --list-pvs
+python -m "${CAPROTO_IOC}" --prefix="BASE:{{Dev:Save1}}:" --list-pvs
